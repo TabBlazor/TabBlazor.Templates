0160af84ec88f1c0073b7bff86c8c0ae5ef5e6ce
Joakim Dangården <Joakim.Dangarden@swegon.com>
2021-09-25 16:13:37 +0200

    Work on Topbar

--- TabBlazor.Templates.Server/Icons/Icons.cs
+++ TabBlazor.Templates.Server/Icons/Icons.cs
@@ -3,5 +3,8 @@
     public static class Icons
     {
         public static string Home { get => @"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><polyline points='5 12 3 12 12 3 21 12 19 12' /><path d='M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7' /><path d='M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6' />"; }
+        public static string Bell { get => @"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6' /><path d='M9 17v1a3 3 0 0 0 6 0v-1' />"; }
+        public static string Search { get => @"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><circle cx='10' cy='10' r='7' /><line x1='21' y1='21' x2='15' y2='15' />"; }
+
     }
 }
